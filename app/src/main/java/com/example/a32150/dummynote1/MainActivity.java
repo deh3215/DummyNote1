20a7a94b8c1836e697c6e5a511fa78c755d24984
deh3215 <3215009@gmail.com>
2017-11-10 11:22:14 +0800

    自訂layout樣式

--- app/src/main/java/com/example/a32150/dummynote1/MainActivity.java
+++ app/src/main/java/com/example/a32150/dummynote1/MainActivity.java
@@ -15,6 +15,11 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    DB db;
+    SimpleCursorAdapter adapter;
+    String[] from;
+    int[] to;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,30 +42,31 @@ public class MainActivity extends AppCompatActivity {
             "Broadcast Receiver"
     };
 
-//    private void setAdapter() {
-//        ListAdapter adapter = new ArrayAdapter<String>(this,
-//                android.R.layout.simple_list_item_1,
-//                note_array);
-//        listView.setAdapter(adapter);
-//
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Toast.makeText(getApplicationContext(), "你選擇的是第" + + (position+1)+"項 : "+note_array[position], Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
-            private DB mDbHelper;
-            private Cursor mNotesCursor;
-            private void setAdapter1() {
-                mDbHelper = new DB(this);
-                mDbHelper.open();
-                mNotesCursor = mDbHelper.getAll();
-                startManagingCursor(mNotesCursor);
-                String[] from = new String[]{"note"};
-                int[] to = new int[]{android.R.id.text1};
-                SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, mNotesCursor, from, to);
-                listView.setAdapter(adapter);
-            }
+        private DB mDbHelper;
+        private Cursor mNotesCursor;
+
+        private void setAdapter1() {
+            mDbHelper = new DB(this);
+            mDbHelper.open();
+            mNotesCursor = mDbHelper.getAll();
+            if(mNotesCursor.getCount() == 0)
+                Toast.makeText(this, "No Data !", Toast.LENGTH_SHORT).show();
+            //android.R.id.xxx ===> 系統預設
+            //R.id.xxx ===> 自訂layout樣式
+            startManagingCursor(mNotesCursor);
+            from = new String[]{"note", "created"};
+            to = new int[]{R.id.tex1, R.id.text2};
+            adapter = new SimpleCursorAdapter(this, R.layout.simple_list_item_1, mNotesCursor, from, to);
+            listView.setAdapter(adapter);
+        }
+
+        void onClick(View v)    {
+            db.open();
+            db.getAll();
+            db.close();
+
+            adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, mNotesCursor, from, to);
+            listView.setAdapter(adapter);
+        }
     }
 
