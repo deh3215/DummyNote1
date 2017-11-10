fbac2a00f307c08e4cf8dd1bd262f7facfb75b35
deh3215 <3215009@gmail.com>
2017-11-10 09:44:17 +0800

    new commit

--- app/src/main/java/com/example/a32150/dummynote1/DB.java
+++ app/src/main/java/com/example/a32150/dummynote1/DB.java
@@ -14,29 +14,25 @@ public class DB {
 
     private static final String DATABASE_NAME="notes.db";
     private static final int DATABASE_VERSION=1;
-    private static final String DATABASE_TABLE="notes";
-    private static final String DATABASE_CREATE="CREATE TABLE notes(_id INTEGER PRIMARY KEY, note TEXT, created INTEGER);";
-    private static final String DATABASE_DROP="DROP TABLE IF EXISTS"+DATABASE_TABLE;
-
-
-
-
+    private static final String DATABASE_TABLE="mytable";
+    private static final String DATABASE_CREATE=
+            "CREATE TABLE IF NOT EXISTS mytable(_id INTEGER PRIMARY KEY, note TEXT NOT NULL, created INTEGER);";
 
     private static class DatabaseHelper extends SQLiteOpenHelper
     {
         public DatabaseHelper(Context context) {
-            super(context, DATABASE_TABLE, null, DATABASE_VERSION);
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
 
         @Override
-        public void onCreate(SQLiteDatabase sqLiteDatabase) {
-            sqLiteDatabase.execSQL(DATABASE_CREATE);
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DATABASE_CREATE);
         }
 
         @Override
-        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-            sqLiteDatabase.execSQL("");
-            onCreate(sqLiteDatabase);
+        public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+            db.execSQL("DROP TABLE IF EXISTS"+DATABASE_TABLE);
+            onCreate(db);
         }
     }
 
@@ -63,6 +59,6 @@ public class DB {
     }
 
     public Cursor getAll()  {
-        return db.rawQuery("SELECT * FROM notes", null);
+        return db.rawQuery("SELECT * FROM mytable", null);
     }
 }
