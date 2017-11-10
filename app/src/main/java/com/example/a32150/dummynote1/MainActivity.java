fe33b3125d3090119cedb9f75a4d571f228b6672
deh3215 <3215009@gmail.com>
2017-11-10 16:29:44 +0800

    增加新增,刪除資料,修改還沒實作

--- app/src/main/java/com/example/a32150/dummynote1/MainActivity.java
+++ app/src/main/java/com/example/a32150/dummynote1/MainActivity.java
@@ -5,6 +5,9 @@ import android.database.Cursor;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,7 +15,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView listView;
     DB db;
@@ -29,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.list);
         listView.setEmptyView(findViewById(R.id.list));
+        listView.setOnItemClickListener(this);
 //        setAdapter();
+
+        mDbHelper = new DB(this);
+        mDbHelper.open();
 
         setAdapter1();
     }
@@ -46,8 +53,7 @@ public class MainActivity extends AppCompatActivity {
         private Cursor mNotesCursor;
 
         private void setAdapter1() {
-            mDbHelper = new DB(this);
-            mDbHelper.open();
+
             mNotesCursor = mDbHelper.getAll();
             if(mNotesCursor.getCount() == 0)
                 Toast.makeText(this, "No Data !", Toast.LENGTH_SHORT).show();
@@ -60,13 +66,46 @@ public class MainActivity extends AppCompatActivity {
             listView.setAdapter(adapter);
         }
 
-        void onClick(View v)    {
-            db.open();
-            db.getAll();
-            db.close();
 
-            adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, mNotesCursor, from, to);
-            listView.setAdapter(adapter);
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,1,0,"新增").setIcon(android.R.drawable.ic_menu_add).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(0,2,0,"刪除").setIcon(android.R.drawable.ic_menu_delete).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(0,3,0,"修改").setIcon(android.R.drawable.ic_menu_edit).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+        return super.onCreateOptionsMenu(menu);
     }
+    int count;
+    int rowId;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //count = mDbHelper.getAll().getCount()-1;
+        switch (item.getItemId())   {
+            case 1:
+                count++;
+                mDbHelper.create(count+".Note");
+                setAdapter1();
+                break;
+            case 2:
+                mDbHelper.delete(rowId);
+                Log.d("Delete", ""+rowId);
+                setAdapter1();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        rowId = i;
+        Toast.makeText(this, "妳按了"+rowId, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
+}
 
